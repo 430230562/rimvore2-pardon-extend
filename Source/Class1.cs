27b04e6957ffe675707eba96c5afc184238ca79a
430230562 <111505575+430230562@users.noreply.github.com>
2025-09-28 22:48:11 +0800

    Add settings support and refactor gene/hediff defs
    Introduced XmlExtensions-based mod settings and Chinese translation keys. Refactored gene and hediff definitions to remove direct capacity modifications and update category naming. Added C# source files and project structure for mod initialization. Removed obsolete .idea and patch files, and updated .gitignore for new development environments.

---
+++ Source/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+
+namespace RV2_PadonExtend
+{
+    public class Main
+    {
+        public Main()
+        {
+            new Harmony("Pardon.RV2Extend").PatchAll();
+        }
+
+        public const string id = "Pardon.RV2Extend";
+
+
+    }
+}
