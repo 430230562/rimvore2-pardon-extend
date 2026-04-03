f101787b5ad26b2b8fb05bc804ccfe1024fc28ec
430230562 <111505575+430230562@users.noreply.github.com>
2026-04-03 23:29:44 +0800

    fix

---
+++ Source/Vore/StagePassCondition.cs
@@ -0,0 +1,18 @@
+﻿using RimVore2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRV2E
+{
+    public class StagePassCondition_Age : TargetedStagePassCondition
+    {
+        public override float AbstractDuration(StageWorker onCycle, StageWorker onStart)
+        {
+            return 100f;
+        }
+
+    }
+}
